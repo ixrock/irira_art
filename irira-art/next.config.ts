1b2e5a0c7922233042802207f49eb380661cc6e6
Roman <ixrock@gmail.com>
2024-10-28 13:13:20 +0200

    chore: allow to import inline-svg files

--- irira-art/next.config.ts
+++ irira-art/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  webpack(config) {
+    // import files as inline SVG-components (XML)
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
