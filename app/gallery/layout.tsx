58eb5b420b9d2615b7868ce9897222b8f6893723
Roman <ixrock@gmail.com>
2024-10-29 13:13:36 +0200

    feat: gallery all categories preview page

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -5,13 +5,6 @@ export default async function GalleryLayout(props: React.PropsWithChildren) {
     <section>
       <h3>My Art</h3>
       {props.children}
-      {/*
-        [TODO]: add gallery previews (up to 10-20 photos):
-        include "oil-painting";
-        include "eco-art";
-        include "eco-jewellery";
-        include "digital-art";
-      */}
     </section>
   );
 };
--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,9 +1,38 @@
 import { Metadata } from "next";
+import { getFilesList } from "@/app/server-actions";
+import Gallery from "@/app/components/gallery";
 
 export const metadata: Metadata = {
   title: "My Art",
 }
 
-export default async function Gallery() {
-  return null
+export default async function GalleryPreviews() {
+  const maxPreviewPerCategory = 5;
+
+  const [oil, digital, ecoArt, ecoJewel, moreArt] = await Promise.all([
+    getFilesList("img/oil_painting"),
+    getFilesList("img/digital_art"),
+    getFilesList("img/eco_art"),
+    getFilesList("img/eco_jewellery/main"),
+    getFilesList("img/more_art/graphics"),
+  ]);
+
+  return (
+    <div className="gallery-previews">
+      <h1>Oil painting</h1>
+      <Gallery files={oil.slice(0, maxPreviewPerCategory)}/>
+
+      <h1>Digital Art</h1>
+      <Gallery files={digital.slice(0, maxPreviewPerCategory)}/>
+
+      <h1>Eco Art</h1>
+      <Gallery files={ecoArt.slice(0, maxPreviewPerCategory)}/>
+
+      <h1>Eco Jewellery</h1>
+      <Gallery files={ecoJewel.slice(0, maxPreviewPerCategory)}/>
+
+      <h1>More art</h1>
+      <Gallery files={moreArt.slice(0, maxPreviewPerCategory)}/>
+    </div>
+  )
 };
