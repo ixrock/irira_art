de3bc4ee44e5930ce8b8466ec4320ff432f26c98
Roman <ixrock@gmail.com>
2024-10-31 14:19:19 +0200

    refactoring: my art page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,14 +1,15 @@
+import styles from "./gallery.module.css";
+import React from "react";
 import { Metadata } from "next";
-import { getFilesList } from "@/app/server-actions";
+import { FileListing, getFilesList } from "@/app/server-actions";
 import Gallery from "@/app/components/gallery";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "My Art",
 }
 
-export default async function GalleryPreviews() {
-  const maxPreviewPerCategory = 5;
-
+export default async function GalleryPage() {
   const [oil, digital, ecoArt, ecoJewel, moreArt] = await Promise.all([
     getFilesList("img/oil_painting"),
     getFilesList("img/digital_art"),
@@ -18,21 +19,34 @@ export default async function GalleryPreviews() {
   ]);
 
   return (
-    <div className="gallery-previews">
-      <h1>Oil painting</h1>
-      <Gallery files={oil.slice(0, maxPreviewPerCategory)}/>
-
-      <h1>Digital Art</h1>
-      <Gallery files={digital.slice(0, maxPreviewPerCategory)}/>
-
-      <h1>Eco Art</h1>
-      <Gallery files={ecoArt.slice(0, maxPreviewPerCategory)}/>
-
-      <h1>Eco Jewellery</h1>
-      <Gallery files={ecoJewel.slice(0, maxPreviewPerCategory)}/>
-
-      <h1>More art</h1>
-      <Gallery files={moreArt.slice(0, maxPreviewPerCategory)}/>
+    <div className={styles.gallery}>
+      <GalleryPreview header="Oil painting" detailsUrl="/gallery/oil-painting" files={oil}/>
+      <GalleryPreview header="Digital Art" detailsUrl="/gallery/digital-art" files={digital}/>
+      <GalleryPreview header="Eco Art" detailsUrl="/gallery/eco-art" files={ecoArt}/>
+      <GalleryPreview header="Eco Jewellery" detailsUrl="/gallery/eco-jewellery" files={ecoJewel}/>
+      <GalleryPreview header="More art" detailsUrl="/gallery/more-art" files={moreArt}/>
     </div>
   )
 };
+
+interface GalleryPreviewProps {
+  header: React.ReactNode;
+  files: FileListing[];
+  detailsUrl?: string;
+  previewCount?: number; /* default: 5 */
+}
+
+function GalleryPreview(props: GalleryPreviewProps) {
+  const { files, previewCount = 5, header, detailsUrl } = props;
+  return (
+    <>
+      <h1>{header}</h1>
+      <Gallery files={files.slice(0, previewCount)}/>
+      {detailsUrl && (
+        <div className={styles.viewAll}>
+          <Link href={detailsUrl}>Show all &rarr;</Link>
+        </div>
+      )}
+    </>
+  )
+}
