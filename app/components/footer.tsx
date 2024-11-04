bbfcb166a82d0a5312e29823e946cedae0f12064
Roman <ixrock@gmail.com>
2024-11-04 10:15:59 +0200

    fix: invisible footer with social icons + added link to tiktok

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,24 +1,24 @@
 import styles from "./footer.module.css"
 import React from "react";
-import Image from "@/node_modules/next/image";
-import { fbLink, instagramLink, vkLink, youtubeLink } from "@/app/config";
+import { fbLink, instagramLink, tiktokLink, vkLink, youtubeLink } from "@/app/config";
 
 export default function Footer() {
-  const iconSize = 25;
-
   return (
     <footer className={styles.socialIcons}>
       <a href={vkLink} target="_blank">
-        <Image src="/vk.svg" width={iconSize} height={iconSize} alt="Vk.com"/>
+        <img src="/vk.svg" alt="Vk.com"/>
       </a>
       <a href={fbLink} target="_blank">
-        <Image src="/facebook.svg" width={iconSize} height={iconSize} alt="Facebook"/>
+        <img src="/facebook.svg" alt="Facebook"/>
       </a>
       <a href={instagramLink} target="_blank">
-        <Image src="/instagram.svg" width={iconSize} height={iconSize} alt="Instagram"/>
+        <img src="/instagram.svg" alt="Instagram"/>
       </a>
       <a href={youtubeLink} target="_blank">
-        <Image src="/youtube.svg" width={iconSize} height={iconSize} alt="YouTube Channel"/>
+        <img src="/youtube.svg" alt="YouTube Channel"/>
+      </a>
+      <a href={tiktokLink} target="_blank">
+        <img src="/tiktok-circle-black.svg" alt="TikTok Channel" className={styles.tikTok}/>
       </a>
     </footer>
   )
--- app/config.ts
+++ app/config.ts
@@ -5,3 +5,4 @@ export const vkLink = "https://vk.com/uniqueartcreations";
 export const fbLink = "https://www.facebook.com/Mudra-Unique-Art-Creations-By-Iri-262591347114916/";
 export const instagramLink = "https://www.instagram.com/iri.ra_art";
 export const youtubeLink = "https://www.youtube.com/@irira4006";
+export const tiktokLink = "https://www.tiktok.com/@iri.ra0";
