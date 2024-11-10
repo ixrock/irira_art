b8f016184e3ff584763df7c17821f9d275f8f8dc
Roman <ixrock@gmail.com>
2024-11-10 11:14:58 +0200

    chore: updating header's image and home page social banners

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import "./css/home.css";
 import React from "react";
 import { Metadata } from "next";
 import { Carousel, CarouselItem } from "@/app/components/carousel";
-import { fbLink, instagramLink, youtubeLink } from "@/app/config";
+import { instagramLink, tiktokLink, youtubeLink } from "@/app/config";
 
 export const metadata: Metadata = {
   title: "Iri Ra - Spiritual artist",
@@ -16,14 +16,14 @@ const carouselItems: CarouselItem[] = [
     linkUrl: "/gallery/eco_art"
   },
   {
+    imageSrc: "/img/home/oil_painting.jpeg",
+    title: "Oil painting",
+    linkUrl: "/gallery/oil_painting"
+  },
+  {
     imageSrc: "/img/home/jewellery.jpg",
     title: "Handcrafted jewellery",
     linkUrl: "/gallery/eco_jewellery"
-  },
-  {
-    imageSrc: "/img/home/oil_painting.jpg",
-    title: "Oil painting",
-    linkUrl: "/gallery/oil_painting"
   }
 ];
 
@@ -42,14 +42,14 @@ export default function Home() {
       <Carousel items={carouselItems}/>
 
       <div className="joinMe">
-        <a href={fbLink} target="_blank">
-          <img src="/img/home/join_me_fb.jpg" alt="Facebook"/>
-        </a>
         <a href={instagramLink} target="_blank">
-          <img src="/img/home/join_me_insta.jpg" alt="Instagram"/>
+          <img src="/img/home/knopka1.jpeg" alt="Follow me on Instagram"/>
+        </a>
+        <a href={tiktokLink} target="_blank">
+          <img src="/img/home/knopka2.jpeg" alt="Follow me on TikTok"/>
         </a>
         <a href={youtubeLink} target="_blank">
-          <img src="/img/home/join_me_youtube.jpg" alt="Youtube"/>
+          <img src="/img/home/knopka3.jpeg" alt="My Videos at Youtube"/>
         </a>
       </div>
     </section>
